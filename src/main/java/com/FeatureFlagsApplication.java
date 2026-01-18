eb350d35234b0d87f8f7d61e2c0c9ff490dd1663
Dmytro Deikun <dmytro.deikun@dotcode.pro>
2026-01-18 19:10:49 +0200

    Initial Commit

---
+++ src/main/java/com/FeatureFlagsApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagsApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FeatureFlagsApplication.class, args);
+    }
+
+}
